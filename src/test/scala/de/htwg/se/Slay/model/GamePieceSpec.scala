d65b30d135bfb9395155e2cf718cafe5781730f7
MRudi95 <marvin.rudolph@htwg-konstanz.de>
2019-04-11 20:58:28 +0200

    fixed packages

---
+++ src/test/scala/de/htwg/se/Slay/model/GamePieceSpec.scala
@@ -0,0 +1,7 @@
+package de.htwg.se.slay.model
+
+import org.scalatest._
+
+class GamePieceSpec extends WordSpec with Matchers {
+
+}
--- src/test/scala/de/htwg/se/Slay/model/PlayerSpec.scala
+++ src/test/scala/de/htwg/se/Slay/model/PlayerSpec.scala
@@ -3,6 +3,7 @@ package de.htwg.se.slay.model
 import org.scalatest._
 
 class PlayerSpec extends WordSpec with Matchers {
+
   "A Player" when {
     "new" should {
       val player = Player("Your Name")
