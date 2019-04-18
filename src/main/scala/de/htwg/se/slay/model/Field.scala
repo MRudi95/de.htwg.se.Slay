ff9dca66434c145a144cbc2c680f43c50e7637f9
MRudi95 <marvin.rudolph@htwg-konstanz.de>
2019-04-18 10:14:23 +0200

    changed Field class changed FieldSpec test

--- src/main/scala/de/htwg/se/slay/model/Field.scala
+++ src/main/scala/de/htwg/se/slay/model/Field.scala
@@ -1,5 +1,19 @@
 package de.htwg.se.slay.model
 
-case class Field(i: Int, str: String, str1: String) {
-  def newOwner(index:Int, color: String, name: String):Field = Field(index, color, name)
+class Field(private var _owner: Player) {
+  private var _gamepiece: GamePiece = _
+  private var _neighbors: Vector[Field] = _
+
+  def owner:Player = _owner
+  def owner_=(owner: Player):Unit = _owner = owner
+
+  def gamepiece:GamePiece = _gamepiece
+  def gamepiece_=(gamepiece: GamePiece):Unit = _gamepiece = gamepiece
+
+  def neighbors:Vector[Field] = _neighbors
+  def setNeighbors(neighbors: Vector[Field]):Boolean = {
+    if(neighbors.length != 4 || _neighbors != null) return false
+    _neighbors = neighbors
+    true
+  }
 }
