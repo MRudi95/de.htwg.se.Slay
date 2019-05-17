8aba54d5098cb4f2c404e325452fe2a05c9ada01
rudi <marvin.rudolph@htwg-konstanz.de>
2019-05-17 14:35:11 +0200

    TextUI tests

--- src/test/scala/de/htwg/se/slay/aview/TextUISpec.scala
+++ src/test/scala/de/htwg/se/slay/aview/TextUISpec.scala
@@ -1,5 +1,29 @@
 package de.htwg.se.slay.aview
 
-class TextUISpec{
+import de.htwg.se.slay.controller.Controller
+import org.scalatest._
 
+class TextUISpec extends WordSpec with Matchers{
+  "A TextUI handles the User Interface in the Console and" when{
+    "new" should{
+      val controller = new Controller
+      val tui = new TextUI(controller)
+
+      "be in the controllers subscribers list" in{
+        controller.subscribers.contains(tui)
+      }
+
+      "print a Welcome Screen" in{
+        tui.welcomeScreen()
+      }
+
+      "process the Welcome" in{
+        tui.processWelcome("yes")
+      }
+
+      "prompt a players name" in{
+        tui.readPlayerName(1)
+      }
+    }
+  }
 }
