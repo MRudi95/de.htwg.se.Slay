f0a3fc00e79e29a99935a24a14b6642a0827ae30
MRudi95 <marvin.rudolph@htwg-konstanz.de>
2019-06-02 12:30:31 +0200

    added highlighting for the fields

--- src/main/scala/de/htwg/se/slay/aview/gui/SwingGUI.scala
+++ src/main/scala/de/htwg/se/slay/aview/gui/SwingGUI.scala
@@ -7,6 +7,7 @@ import javax.swing.ImageIcon
 
 import scala.swing._
 import scala.swing.Swing.LineBorder
+import scala.swing.event.MouseClicked
 
 class SwingGUI(controller: Controller) extends Frame with Observer{
   val iconLoc = "src/main/resources/icons/"
@@ -14,6 +15,7 @@ class SwingGUI(controller: Controller) extends Frame with Observer{
   title = "Slay Scala"
 
   def gridPanel: GridPanel = new GridPanel(controller.grid.rowIdx+1, controller.grid.colIdx+1){
+    var idx = 0
     for{f <- controller.grid} {
       contents += new Label(){
         border = LineBorder(java.awt.Color.BLACK, 1)
@@ -28,9 +30,21 @@ class SwingGUI(controller: Controller) extends Frame with Observer{
           case _:Tree     => new ImageIcon(iconLoc + "tree.png")
           case _:Capital  => new ImageIcon(iconLoc + "capital.png")
         }
+        name = idx.toString
         opaque = true
         preferredSize = new Dimension(64, 64)
+
+        listenTo(mouse.clicks)
+        reactions += {
+          case mc:MouseClicked =>
+            mc.peer.getButton match {
+              case java.awt.event.MouseEvent.BUTTON1 => border = LineBorder(java.awt.Color.WHITE, 2)
+              case java.awt.event.MouseEvent.BUTTON3 => border = LineBorder(java.awt.Color.BLACK, 1)
+              case _ =>
+          }
+        }
       }
+      idx += 1
     }
   }
 
