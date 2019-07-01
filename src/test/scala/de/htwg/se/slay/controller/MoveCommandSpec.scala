b266f6a92398305a46e346cfe4f08b3133c6bc61
MRudi95 <marvin.rudolph@htwg-konstanz.de>
2019-07-01 23:52:02 +0200

    added MoveCommandSpec tests

--- src/test/scala/de/htwg/se/slay/controller/MoveCommandSpec.scala
+++ src/test/scala/de/htwg/se/slay/controller/MoveCommandSpec.scala
@@ -8,10 +8,21 @@ class MoveCommandSpec extends WordSpec with Matchers{
     val ctrl = new Controller
     ctrl.createGrid("Map1")
     BuyCommand(8, ctrl).doStep()
+    BuyCommand(12, ctrl).doStep()
 
     "move a Unit from one field to another " in{
       new MoveCommand(ctrl.grid(8), ctrl.grid(24), ctrl).doStep()
       //ctrl.grid(24).gamepiece should be (classOf[Peasant])
+    }
+
+    "undo and redo the Command" in{
+      val mc = new MoveCommand(ctrl.grid(12), ctrl.grid(28), ctrl)
+
+      mc.doStep()
+
+      mc.undoStep()
+
+      mc.redoStep()
     }
   }
 }
