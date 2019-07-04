06ca8cbc5f0df8f492ea71158ebfa97ee4349913
MRudi95 <marvin.rudolph@htwg-konstanz.de>
2019-07-04 20:50:06 +0200

    tests

--- src/test/scala/de/htwg/se/slay/model/fileIOComponent/fileIoXMLimpl/FileIOSpec.scala
+++ src/test/scala/de/htwg/se/slay/model/fileIOComponent/fileIoXMLimpl/FileIOSpec.scala
@@ -1,7 +1,15 @@
 package de.htwg.se.slay.model.fileIOComponent.fileIoXMLimpl
 
+import de.htwg.se.slay.controller.controllerComponent.controllerMockImpl.Controller
 import org.scalatest._
 
 class FileIOSpec extends WordSpec with Matchers {
-
+  "JSON FileIO" should{
+    "do stuff" in{
+      val xmlIO = new FileIO
+      val ctrl = new Controller
+      val (p1, p2, s, g) = xmlIO.load("test", ctrl.players)
+      xmlIO.save("test", ctrl.players, s, g)
+    }
+  }
 }
